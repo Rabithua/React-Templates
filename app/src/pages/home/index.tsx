727fcfdec08644cc18ed18e5ce82ce217b3b2b0a
Rabithua <rabithua@gmail.com>
2023-07-10 16:48:27 +0800

    README

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -3,7 +3,7 @@ function Home() {
       <div className=" h-screen w-full flex justify-center items-center">
         <h1 className="text-3xl mx-4 font-mono font-thin text-emerald-600">
           React + Typescript + Tailwind + React router + React hot toast Here💫
-          <div className=" text-sm text-center my-4 text-gray-400">🤖 JUST CHANGE ./app/App.tsx AND START YOUR MASTERPIECE</div>
+          <div className=" text-sm text-center my-4 text-gray-400">🤖 JUST CHANGE ./app/src/pages/home/index.tsx AND START YOUR MASTERPIECE</div>
         </h1>
       </div>
     );
