95da1d6e49b70664f2db43bec8c444b3a719099f
Rabithua <34543831+Rabithua@users.noreply.github.com>
2023-07-10 18:23:50 +0800

    Update index.tsx

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -5,17 +5,17 @@ export default function ErrorPage() {
           <div className="text-center">
             <p className=" text-[200px] font-semibold text-indigo-600">404</p>
             <h1 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 ">
-              页面未找到或者出现了错误
+              Page not found or an error occurred.
             </h1>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              抱歉，我们无法找到您正在查找的页面。
+              Sorry, we couldn't find the page you were looking for.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="/"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                返回首页
+                Take me home.
               </a>
               {/* <a href="#" className="text-sm font-semibold text-gray-900">
                 Contact support <span aria-hidden="true">&rarr;</span>
